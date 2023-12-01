e0cc4468af2a936267c6637cd8821c0e9a8b4d01
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-12-01 22:40:18 +0300

    Innitial Commit

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Todays Task */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>
+          Today's tasks
+        </Text>
+        <View style={styles.items}>
+                  {/* This is where the tasks items will go */}
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e8eaed',    
   },
+  taskWrapper: {
+
+  },
+  sectionTitle: {
+
+  },
+  items: {
+
+  }
 });
